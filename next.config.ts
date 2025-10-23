ba9c8eeb5ee2a86ee4dc75bf4753235ea4603007
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-10-23 12:25:59 +0300

    upd next

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
   webpack(config) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     };
     return config;
   },
