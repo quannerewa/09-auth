5551784aea2f0be05994e295b71ec71545a4c96e
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-10-24 10:06:10 +0300

    upd

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -5,3 +5,12 @@ export const nextServer = axios.create({
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
 });
+export async function GET() {
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: {
+          "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  })
+}
