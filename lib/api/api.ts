101cf9c937263b4e14cbdf605d71a537d2e48f46
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-10-26 20:57:44 +0200

    upd api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "https://09-auth-kappa-taupe.vercel.app";
-
 export const nextServer = axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: "/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
