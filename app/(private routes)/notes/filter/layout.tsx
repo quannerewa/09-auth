f9a1cfb9f6934e1bae7b4755964a0a2d1102a9e0
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-10-26 23:46:31 +0200

    Update layout.tsx

--- app/(private routes)/notes/filter/layout.tsx
+++ app/(private routes)/notes/filter/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import css from "./layoutNotes.module.css";
+import css from "notes/filter/layoutNotes.module.css";
 interface NotesLayoutProps {
   children: ReactNode;
   sidebar: ReactNode;
